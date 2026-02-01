d3abe4303753d49a903e039d8650dad4c040a4ee
ellensilvaaat <ellensilvaeng@gmail.com>
2026-02-01 15:01:08 +1100

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // ✅ Gera sourcemaps só em desenvolvimento
     sourcemap: process.env.NODE_ENV !== 'production',
-
-    // ✅ Garante minificação com esbuild (padrão do Vite)
     minify: 'esbuild',
-
-    // ✅ Garante que os arquivos de build vão pra pasta esperada pela Vercel
     outDir: 'dist'
   }
 })
